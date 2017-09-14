674c54882974424ee38d4529630e896f2080be70
Ian <ianburrows.sa@gmail.com>
2017-09-14 15:41:57 +0200

    Created class color and stack

--- CodingDojo02/CodingDojo02/Program.cs
+++ CodingDojo02/CodingDojo02/Program.cs
@@ -8,8 +8,65 @@ namespace CodingDojo02
 {
     class Program
     {
+        public class Color
+        {
+            public string Name;
+
+            public Color(string Name)
+            {
+                this.Name = Name;
+            }
+
+            //public string Name { get; set; }
+
+        }
+
+        public class Stack
+        {
+            public Stack()
+            {
+                
+            }
+
+            public Boolean Push()
+            {
+                return true;
+            }
+
+            public Boolean Pop()
+            {
+                return true;
+            }
+
+            public String Peek()
+            {
+                return "";
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            Stack theStack = new Stack();
+            Console.WriteLine("Enter a color to push to the stack:");
+            Color col = new Color(Console.ReadLine());
+            
+
+
+
         }
+
+        public static String[] createStack()
+        {
+            String[] stack = new String[10];
+            return stack;
+        }
+
+        public void push(String name)
+        {
+            
+        }
+
     }
 }
+
