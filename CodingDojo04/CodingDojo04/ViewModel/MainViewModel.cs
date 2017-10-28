79d87f2cc884c2a4274299080f79e62f0cc41ce1
Ian <ianburrows.sa@gmail.com>
2017-10-28 13:08:23 +0200

    Start of CD 04 and mvvm light implemented

---
+++ CodingDojo04/CodingDojo04/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+using GalaSoft.MvvmLight;
+
+namespace CodingDojo04.ViewModel
+{
+
+    public class MainViewModel : ViewModelBase
+    {
+
+        public MainViewModel()
+        {
+           
+        }
+    }
+}
