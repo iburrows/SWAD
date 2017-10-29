8345f26d271a4b3e8406c880ae7a5ff5ba1e7399
Ian <ianburrows.sa@gmail.com>
2017-10-29 10:31:46 +0100

    binding to firstname, lastname, ssn, date and add button. Add button clickable when lastname > 3 all completed.

--- CodingDojo04/CodingDojo04/ViewModel/MainViewModel.cs
+++ CodingDojo04/CodingDojo04/ViewModel/MainViewModel.cs
@@ -1,14 +1,72 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
 
 namespace CodingDojo04.ViewModel
 {
 
     public class MainViewModel : ViewModelBase
     {
+        private string firstName = "";
+        private string lastName = "";
+        private int sSn;
+        private string date;
+        private bool isClickable;
+
+        RelayCommand clickBtnCommand;
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public bool IsClickable
+        {
+            get { return isClickable; }
+            set { isClickable = value; }
+        }
+
+        public int Ssn
+        {
+            get { return sSn; }
+            set { sSn = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public RelayCommand ClickBtnCommand { get => clickBtnCommand; set => clickBtnCommand = value; }
 
         public MainViewModel()
         {
-           
+            //ClickBtnCommand = new RelayCommand(new Action(ExecuteAdd), new Func<bool>(CanExecuteAdd));
+            ClickBtnCommand = new RelayCommand(new Action(ExecuteAdd), () => { if (LastName.Length > 2) { return true; } else { return false; } });
+        }
+
+        private bool CanExecuteAdd()
+        {
+            if (LastName.Length > 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void ExecuteAdd()
+        {
+            //add logic to add the data to the list
+            throw new NotImplementedException();
         }
     }
 }
