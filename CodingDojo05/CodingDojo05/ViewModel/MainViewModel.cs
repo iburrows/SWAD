0d7af1229072bfeebcea03729a78a8b1200c126a
Ian <ianburrows.sa@gmail.com>
2017-11-10 11:22:44 +0100

    Installed MVVM Light

---
+++ CodingDojo05/CodingDojo05/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+using GalaSoft.MvvmLight;
+
+namespace CodingDojo05.ViewModel
+{
+
+    public class MainViewModel : ViewModelBase
+    {
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
