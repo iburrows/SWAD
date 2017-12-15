df234667f49518436646c821d42a5f1625119ba7
Ian <ianburrows.sa@gmail.com>
2017-12-15 12:55:59 +0100

    Items complete

---
+++ CodingDojo07/CodingDojo07/ViewModel/InventoryVM.cs
@@ -0,0 +1,36 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CodingDojo07.ViewModel
+{
+    public class InventoryVM : ViewModelBase
+    {
+        public string Description { get; set; }
+        public string AgeRestriction { get; set; }
+        public BitmapImage Image { get; set; }
+        public ObservableCollection<InventoryVM> Items { get; set; }
+
+        public InventoryVM(string description, string ageRestriction, BitmapImage image)
+        {
+            this.Description = description;
+            this.AgeRestriction = ageRestriction;
+            this.Image = image;
+        }
+
+        public void AddItem(InventoryVM item)
+        {
+            if (Items == null)
+            {
+                Items = new ObservableCollection<InventoryVM>();
+            }
+
+            Items.Add(item);
+        }
+    }
+}
--- CodingDojo07/CodingDojo07/ViewModel/OverviewVM.cs
+++ CodingDojo07/CodingDojo07/ViewModel/OverviewVM.cs
@@ -13,20 +13,46 @@ namespace CodingDojo07.ViewModel
     {
 
         //private ObservableCollection<Items> items;
-        public ObservableCollection<Items> Items { get; set; }
+        public ObservableCollection<InventoryVM> Items { get; set; }
 
-        
+        private InventoryVM selectedInventory;
+
+        public InventoryVM SelectedInventory
+        {
+            get { return selectedInventory; }
+            set { selectedInventory = value; RaisePropertyChanged(); }
+        }
+
+        private InventoryVM selectedItem;
+
+        public InventoryVM SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value; RaisePropertyChanged(); }
+        }
 
         public OverviewVM()
         {
-            Items = new ObservableCollection<Items>();
+            Items = new ObservableCollection<InventoryVM>();
             GenerateDate();
         }
 
         private void GenerateDate()
         {
-            Items.Add(new Items("Lego", new BitmapImage(new Uri("../Images/Lego/download.jpg", UriKind.Relative))));
-            Items.Add(new Items("Playmobil", new BitmapImage(new Uri("../Images/PlayMobil/playmobil.jpg", UriKind.Relative))));
+            Items.Add(new InventoryVM("Lego", "-" ,new BitmapImage(new Uri("../Images/Lego/download.jpg", UriKind.Relative))));
+            Items.Add(new InventoryVM("Playmobil", "-" ,new BitmapImage(new Uri("../Images/PlayMobil/playmobil.jpg", UriKind.Relative))));
+
+            Items[0].AddItem(new InventoryVM("batman", "5+", new BitmapImage(new Uri("../Images/Lego/batman.jpg", UriKind.Relative))));
+            Items[0].AddItem(new InventoryVM("car", "5+", new BitmapImage(new Uri("../Images/Lego/car.jpg", UriKind.Relative))));
+            Items[0].AddItem(new InventoryVM("ninja", "10+", new BitmapImage(new Uri("../Images/Lego/ninja.jpg", UriKind.Relative))));
+            Items[0].AddItem(new InventoryVM("police", "2+", new BitmapImage(new Uri("../Images/Lego/police.jpg", UriKind.Relative))));
+            Items[0].AddItem(new InventoryVM("shark", "2+", new BitmapImage(new Uri("../Images/Lego/shark.jpg", UriKind.Relative))));
+
+            Items[1].AddItem(new InventoryVM("ark", "2+", new BitmapImage(new Uri("../Images/PlayMobil/ark.jpg", UriKind.Relative))));
+            Items[1].AddItem(new InventoryVM("boat", "5+", new BitmapImage(new Uri("../Images/PlayMobil/boat.jpg", UriKind.Relative))));
+            Items[1].AddItem(new InventoryVM("castle", "3+", new BitmapImage(new Uri("../Images/PlayMobil/castle.jpg", UriKind.Relative))));
+            Items[1].AddItem(new InventoryVM("dragons", "10", new BitmapImage(new Uri("../Images/PlayMobil/dragons.jpg", UriKind.Relative))));
+            Items[1].AddItem(new InventoryVM("farm", "5", new BitmapImage(new Uri("../Images/PlayMobil/farm.jpg", UriKind.Relative))));
         }
     }
 }
