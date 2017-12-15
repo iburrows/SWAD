62d658c5edb23393ae385e53c914c99c0586d44b
Ian <ianburrows.sa@gmail.com>
2017-12-15 11:44:46 +0100

    Overview VM first phase

---
+++ CodingDojo07/CodingDojo07/ViewModel/Items.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CodingDojo07.ViewModel
+{
+    public class Items : ViewModelBase
+    {
+        public string Description { get; set; }
+        //public string AgeRestriction { get; set; }
+        public BitmapImage Image { get; set; }
+
+        public Items(string description, BitmapImage image)
+        {
+            this.Description = description;
+            //this.AgeRestriction = ageRestriction;
+            this.Image = image;
+        }
+    }
+}
--- CodingDojo07/CodingDojo07/ViewModel/OverviewVM.cs
+++ CodingDojo07/CodingDojo07/ViewModel/OverviewVM.cs
@@ -1,13 +1,32 @@
 ﻿using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace CodingDojo07.ViewModel
 {
     public class OverviewVM : ViewModelBase
     {
+
+        //private ObservableCollection<Items> items;
+        public ObservableCollection<Items> Items { get; set; }
+
+        
+
+        public OverviewVM()
+        {
+            Items = new ObservableCollection<Items>();
+            GenerateDate();
+        }
+
+        private void GenerateDate()
+        {
+            Items.Add(new Items("Lego", new BitmapImage(new Uri("../Images/Lego/download.jpg", UriKind.Relative))));
+            Items.Add(new Items("Playmobil", new BitmapImage(new Uri("../Images/PlayMobil/playmobil.jpg", UriKind.Relative))));
+        }
     }
 }
